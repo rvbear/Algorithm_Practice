3cd41b37379a46bb9fb816c244ab0abc3a2154fd
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-03-18 21:39:18 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2788 ms, Memory: 345428 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   // 버퍼 스트림을 통해 입력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));     // 버퍼 스트림을 통해 출력
+
+        int N = Integer.parseInt(br.readLine());        // 수의 개수를 입력
+
+        int[] array = new int[N];   // 수를 저장할 배열 선언
+
+        for(int i = 0; i < N; i++)        // N번만큼 반복
+            array[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(array);     // 배열을 오름차순으로 정렬
+
+        for(int j = 0; j < N; j++) {
+            bw.write(String.valueOf(array[j]));
+            bw.newLine();
+        }
+
+        br.close();         // 입력 스트림 닫아줌
+        bw.close();         // 출력 스트림 닫아줌
+    }
+}
