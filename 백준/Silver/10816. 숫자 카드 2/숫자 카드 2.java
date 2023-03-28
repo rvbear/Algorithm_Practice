4f73077add7583214c288351e295f9bbe8e22e86
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-03-29 00:29:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1308 ms, Memory: 187316 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 출력
-        StringBuilder stb = new StringBuilder();            // StringBuilder를 사용해 출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));     // 버퍼 스트림을 통해 출력
 
         int N = Integer.parseInt(br.readLine());                    // 상근이가 가지고 있는 카드의 개수
         HashMap<Integer, Integer> map = new HashMap<>();            // HashMap을 사용
         StringTokenizer stk = new StringTokenizer(br.readLine());   // StringTokenizer를 사용하여 분해
         for(int i = 0; i < N; i++) {
             int card = Integer.parseInt(stk.nextToken());
-            if(map.containsKey(card) == false)                      // 값이 존재하지 않는다면
+            if(!map.containsKey(card))                              // 값이 존재하지 않는다면
                 map.put(card, 1);                                   // value에 1을 저장
             else                                                    // 값이 존재한다면
                 map.put(card, map.get(card) + 1);                   // 원래 value값에 1을 더한 값을 저장
@@ -21,13 +21,13 @@ public class Main {
         stk = new StringTokenizer(br.readLine());                   // StringTokenizer를 사용해 분해
         for(int j = 0; j < M; j++) {                                // M번 반복
             int find = Integer.parseInt(stk.nextToken());
-            if (map.containsKey(find) == true)                      // 상근이가 카드를 가지고 있다면
-                stb.append(map.get(find)).append(" ");              // 가지고 있는 개수와 공백을 StringBuilder에 저장
+            if (map.containsKey(find))                      // 상근이가 카드를 가지고 있다면
+                bw.write(String.valueOf(map.get(find)) + " ");
             else                                                    // 카드를 가지고 있지 않다면
-                stb.append(0).append(" ");                          // 0과 공백을 StringBuilder에 저장
+                bw.write(0 + " ");
         }
 
-        System.out.println(stb);            // StringBuilder에 저장된 값을 출력
         br.close();                         // 입력 버퍼 스트림 닫아줌
+        bw.close();                         // 출력 버퍼 스트림 닫아줌
     }
 }
