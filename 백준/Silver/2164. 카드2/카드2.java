38199b0f91549738ec1f47ea3cb07dbee004538a
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-04-04 23:04:47 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45692 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -10,17 +10,17 @@ public class Main {
 
         Queue<Integer> queue = new LinkedList<>();      // 카드를 입력할 큐 선언
 
-        for(int i = 1; i <= N; i++)                     // 배열에 값을 초기화
+        for(int i = 1; i <= N; i++)                     // 큐에 값을 넣어줌
             queue.add(i);
 
-        for(int i = 1; queue.size() > 1; i++) {         // 배열에 크기가 1이 될 때까지 진행
-            if(i % 2 == 1)                              // 홀수번째 진행이라면 배열에서 제거
+        for(int i = 1; queue.size() > 1; i++) {         // 큐의 크기가 1이 될 때까지 진행
+            if(i % 2 == 1)                              // 홀수번째 진행이라면 큐에서 제거
                 queue.poll();
             else                                        // 짝수번째 진행이라면 맨 첫번째 값을 맨 뒤로 저장
                 queue.add(queue.poll());
         }
 
-        bw.write(String.valueOf(queue.poll()));           // 배열에 남은 값을 출력
+        bw.write(String.valueOf(queue.poll()));           // 큐에 남은 값을 출력
         bw.close();     // 출력 버퍼 스트림 닫아줌
         br.close();     // 입력 버퍼 스트림 닫아줌
     }
