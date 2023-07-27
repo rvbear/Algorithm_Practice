af853b5cea10cb06a6b34d1e18497d7ef55d1cb6
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2023-07-27 21:47:34 +0900

    [Silver II] Title: 타일 위의 원, Time: 412 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1709. 타일 위의 원/타일 위의 원.java
+++ 백준/Silver/1709. 타일 위의 원/타일 위의 원.java
@@ -1,33 +1,24 @@
 import java.io.*;
 
 public class Main {
-    static long area;
-    public static boolean check(long x, long y) {             // 원의 둘레가 지나가는지 여부
-        return (x-1)*(x-1)+(y-1)*(y-1) < area && x*x + y*y > area;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           // 버퍼 스트림을 통해 입력
-        int n = Integer.parseInt(br.readLine());            // 한 변의 길이
-        int radius = n/2;                                   // 반지름
-        area = (long)radius*radius;                               // 원의 넓이
-        int temp = 1;                                       // x의 시작
-        int count = 0;                                      // 둘레가 지나는 사각형의 개수
+        long n = Long.parseLong(br.readLine());             // 반지름
+        n /= 2;
 
-        for(int y = radius; y >= 0; y--) {                  // 1사분면 탐색
-            for(int x = temp; x <= radius; x++) {
-                if(check(x, y)) {                           // check()를 만족한다면
-                    count++;                                // 개수 증가
-                    temp = x;                               // temp 바꾸기
-                } else {
-                    if(x != temp) {                         // 만족하지 않는다면
-                        temp = x-1;                         // temp 값 변경
-                        break;
-                    }
-                }
+        long x = 0, y = n-1, count = 0;                     // x, y의 시작좌표, 원의 둘레가 지나가는 칸의 개수
+        while(x <= n && y >= 0) {                           // 1사분면만 탐색
+            long dif = (x+1)*(x+1) + y*y;                   // (x+1, y)의 길이
+            if(dif <= n*n) {                                // 원주 안에 있을 경우
+                x++;                                        // x 증가
             }
+            if(dif >= n*n) {                                // 원주 밖에 있을 경우
+                y--;                                        // y 감소
+            }
+            count++;                                        // 칸의 개수 증가
         }
 
-        System.out.println(count*4);                        // 값 출력하기
+        System.out.println(count*4);                        // 1사분면 탐색 결과 * 4
         br.close();                                         // 입력 버퍼 스트림 닫아줌
     }
 }
