ec24a49cf61fff8346bd7482321995c88d5631b5
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-01-13 23:55:25 +0900

    [level Lv.2] Title: 할인 행사, Time: 0.15 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/Lv.2/131127. 할인 행사/README.md
+++ 프로그래머스/Lv.2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level Lv.2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 0.11 ms
+메모리: 90 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 23:13:47
+2024년 1월 6일 23:55:24
 
 ### 문제 설명
 
--- 프로그래머스/Lv.2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/Lv.2/131127. 할인 행사/할인 행사.java
@@ -3,17 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        boolean pass = false;
         
         for(int i = 0; i <= discount.length - 10; i++) {
             String[] arr = Arrays.copyOfRange(discount, i, i+10);
             boolean flag = false;
-            for(int j = 0; j < want.length; j++) {
-                int num = Collections.frequency(Arrays.asList(arr), want[j]);
-                if(number[j] != num) {
+            
+            if(!pass) {
+                for(int j = 0; j < want.length; j++) {
+                    int num = Collections.frequency(Arrays.asList(arr), want[j]);
+                    if(number[j] != num) {
+                        flag = true;
+                        break;
+                    }
+                }
+            } else {
+                if(!discount[i-1].equals(arr[arr.length-1])) {
+                    pass = false;
                     flag = true;
-                    break;
                 }
             }
+            
+            if(!flag) {
+                pass = true;
+                answer++;
+                continue;
+            }
+            
             answer += flag ? 0 : 1;
         }
         
