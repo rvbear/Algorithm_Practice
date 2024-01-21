36b0423a419183df999ff7988a5a8a93c8ecb7d0
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-01-21 18:21:44 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,18 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        int count = 0;
         
+				// HashMap을 사용
         HashMap<String, Integer> map = new HashMap<>();
         for(int i = 0; i < clothes.length; i++) {
+						// 분류를 키값으로 가지고 있다면 value + 1, 아니라면 0 + 1을 해줌
             map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
-            count++;
         }
         
+				// 모든 key값을 조회
         for(String key : map.keySet()) {
+						// 결과에 value + 1을 곱해줌
             answer *= (map.get(key) + 1);
         }
         
+				// 최종 결과값 - 1을 해줌
         return answer - 1;
     }
 }
