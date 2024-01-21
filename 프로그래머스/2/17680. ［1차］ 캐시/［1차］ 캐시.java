f03fef2ed6488c81bfc256e4b8837bfe7b5eb380
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-01-22 05:53:06 +0900

    [level 2] Title: [1차] 캐시, Time: 1.68 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        ArrayList<String> list = new ArrayList<>();
+        
+        for(int i = 0; i < cities.length && cacheSize > 0; i++) {
+            if(list.indexOf(cities[i].toLowerCase()) != -1 && list.size() != 0) {
+                list.add(list.remove(list.indexOf(cities[i].toLowerCase())));
+                answer += 1;
+            } else if(list.size() < cacheSize) {
+                list.add(cities[i].toLowerCase());
+                answer += 5;
+            } else {
+                list.remove(0);
+                list.add(cities[i].toLowerCase());
+                answer += 5;
+            }
+        }
+        
+        return answer = answer > 0 ? answer : 5 * cities.length;
+    }
+}
