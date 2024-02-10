39275cd96465b8a359a1db2fb4699d97f5416a57
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-02-10 13:42:31 +0900

    [level 2] Title: 롤케이크 자르기, Time: 174.82 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,22 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Map<Integer, Integer> left = new HashMap<>();
-        Set<Integer> right = new HashSet<>();
+		// 자른 것을 기준으로 오른쪽을 뜻하는 Map
+        Map<Integer, Integer> right = new HashMap<>();
+        // 자른 것을 기준으로 왼쪽을 뜻하는 Set
+		Set<Integer> left = new HashSet<>();
         
+		// 오른쪽에 모든 값을 다 저장
         for(int t : topping) {
-            left.put(t, left.getOrDefault(t, 0) + 1);
+            right.put(t, right.getOrDefault(t, 0) + 1);
         }
         
+		// 맨 처음부터 하나씩 잘라가면서 비교
         for(int t : topping) {
-            left.put(t, left.get(t) - 1);
-            right.add(t);
+            right.put(t, right.get(t) - 1);
+            left.add(t);
             
-            if(left.get(t) == 0) {
-                left.remove(t);
+			// 해당 토핑이 존재하지 않을 경우 삭제
+            if(right.get(t) == 0) {
+                right.remove(t);
             }
             
-            if(left.size() == right.size()) {
+			// 왼쪽과 오른쪽의 토핑 수가 같으면 answer 증가
+            if(right.size() == left.size()) {
                 answer++;
             }
         }
