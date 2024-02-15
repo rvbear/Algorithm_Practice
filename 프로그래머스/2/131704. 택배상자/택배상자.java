b0182f14e620d49d4687314c4889dbdf6aecc6de
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-02-15 19:24:07 +0900

    [level 2] Title: 택배상자, Time: 162.20 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -6,29 +6,14 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         
         for(int i = 1, j = 0; i <= order.length; i++) {
-            boolean flag = false;
-            if(order[j] == i) {
-                answer++;
-                j++;
-                flag = true;
-            }
+            stack.add(i);
             while(!stack.isEmpty() && stack.peek() == order[j]) {
                 stack.pop();
                 answer++;
                 j++;
             }
-            if(!flag) {
-                stack.add(i);
-            }
         }
-        /*
-        1 2 3 4 5
-        1  2345
-        12 345
-        123 45
-        4 123 5
-        43 12 5
-        */  
+        
         return answer;
     }
 }
