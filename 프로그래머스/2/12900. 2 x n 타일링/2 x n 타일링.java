4214dd768deb9b22bccbe19b03011ef98f220e72
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-02-17 21:39:48 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.50 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[2] = 2;
+        dp[3] = 3;
+        
+        for(int i = 4; i < n+1; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        
+        return dp[n];
+    }
+}
