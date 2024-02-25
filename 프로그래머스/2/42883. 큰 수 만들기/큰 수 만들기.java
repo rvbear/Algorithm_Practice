3680b857e16af6955185a6853ff5d66ccdd40a8d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-02-25 14:14:18 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String number, int k) {
+        int index = 0;
+        StringBuilder answer = new StringBuilder("");
+        
+        for(int i = 0; i < number.length() - k; i++) {
+            int max = 0;
+            for(int j = index; j <= k+i; j++) {
+                if(max < number.charAt(j) - '0') {
+                    max = number.charAt(j) - '0';
+                    index = j+1;
+                }
+            }
+            answer.append(max);
+        }
+        
+        return answer.toString();
+    }
+}
