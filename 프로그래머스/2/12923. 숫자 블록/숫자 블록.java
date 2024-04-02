2769395b82f3deee1e8487b99e57d2071a152f4d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-04-03 02:03:27 +0900

    [level 2] Title: 숫자 블록, Time: 165.21 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(long begin, long end) {
+        int start = (int)begin, last = (int)end, index = 0;
+        int[] answer = new int[last - start + 1];
+        
+        for(int i = start; i <= last; i++) {
+            if (i == 1) {
+                answer[index++] = 0;
+                continue;
+            }
+
+            boolean flag = false;
+            
+            List<Integer> list = new ArrayList<>();
+
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    list.add(j);
+                    if (i / j <= 10000000) {
+                        answer[index++] = i / j;
+                        flag = true;
+                        break;
+                    }
+                }
+             }
+            
+            if(flag) {
+                continue;
+            }
+            
+            if (!list.isEmpty()) {
+                answer[index++] = list.get(list.size() - 1);
+                continue;
+            }
+
+            answer[index++] = 1;
+        }
+        
+        return answer;
+    }
+}
