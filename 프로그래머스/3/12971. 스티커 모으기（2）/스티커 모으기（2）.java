24e25b59b15afd5d4da43559058a681fab03b90d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-04-27 20:42:30 +0900

    [level 3] Title: 스티커 모으기(2), Time: 5.60 ms, Memory: 56 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int answer;
+        int[] dp = new int[sticker.length];
+        
+        if(sticker.length == 1) {
+            return sticker[0];
+        }
+        
+        dp[1] = dp[0] = sticker[0];
+        for(int i = 2; i < sticker.length - 1; i++) {
+            dp[i] = Math.max(dp[i-2] + sticker[i], dp[i-1]);
+        }
+        answer = dp[sticker.length - 2];
+
+        dp[0] = 0;
+        dp[1] = sticker[1];
+        for(int i = 2; i < sticker.length; i++) {
+            dp[i] = Math.max(dp[i-2] + sticker[i], dp[i-1]);
+        }
+        
+        return Math.max(answer, dp[sticker.length - 1]);
+    }
+}
