a78029f47e47597ff143cd7462b2189955300a70
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-10-22 23:56:08 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 15.11 ms, Memory: 96.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        Map<String, Integer> friendMap = new HashMap<>();
+        int[] giftDegree = new int[friends.length];
+        int[][] giftGraph = new int[friends.length][friends.length];
+        
+        for(int i = 0; i < friends.length; i++) {
+            friendMap.put(friends[i], i);
+        }
+        
+        for(String gift : gifts) {
+            String[] name = gift.split(" ");
+            giftDegree[friendMap.get(name[0])]++;
+            giftDegree[friendMap.get(name[1])]--;
+            giftGraph[friendMap.get(name[0])][friendMap.get(name[1])]++;
+        }
+        
+        for(int i = 0; i < friends.length; i++) {
+            int num = 0;
+            for(int j = 0; j < friends.length; j++) {
+                if(i == j) continue;
+                
+                if(giftGraph[i][j] > giftGraph[j][i] || (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
+                    num++;
+                }
+            }
+            
+            if(answer < num) {
+                answer = num;
+            }
+        }
+        
+        return answer;
+    }
+}
