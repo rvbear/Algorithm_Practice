55bf9c0c3e661dbe54acb0df7512b48eebed9a8e
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2024-12-01 21:06:48 +0900

    Time: 2 ms (93.52%), Space: 43 MB (42.13%) - LeetHub

---
+++ LeetCode/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : arr) {
+            if(set.contains(n * 2) || (n % 2 == 0 && set.contains(n / 2))) {
+                return true;
+            }
+
+            set.add(n);
+        }
+
+        return false;
+    }
+}
