96c3ebf7155d19795571e4e079d1dd60d0a8d936
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-01-04 23:13:36 +0900

    [level 3] Title: 억억단을 외우자, Time: 316.42 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,46 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int e, int[] starts) {
-        int[][] dp = new int[e+1][2];
-        
-        for(int i = 0; i < e+1; i++) {
-            dp[i][0] = i;
-        }
+        int[] answer = new int[starts.length];
+        int[] count = new int[e+1];
         
         for(int i = 1; i < e+1; i++) {
-            for(int j = i; j < e+1; j += i) {
-                dp[j][1]++;
+            for(int j = 1; j <= e/i; j++) {
+                count[i*j]++;
             }
         }
+
+        int[] max = new int[e+1];
+        max[e] = e;
         
-        Arrays.sort(dp, new Comparator<int[]>() {
-           @Override
-            public int compare(int[] a, int[] b) {
-                if(a[1] < b[1]) {
-                    return 1;
-                } else if(a[1] > b[1]) {
-                    return -1;
-                } else {
-                    if(a[0] > b[0]) {
-                        return 1;
-                    } else if(a[0] < b[0]) {
-                        return -1;
-                    } else {
-                        return 0;
-                    }
-                }
-            }
-        });
-        
-        int[] answer = new int[starts.length];
-        for(int i = 0; i < starts.length; i++) {
-            for(int j = 0; j < e+1; j++) {
-                if(starts[i] <= dp[j][0]) {
-                    answer[i] = dp[j][0];
-                    break;
-                }
-            }
+        for(int i = e-1; i > 0; i--) {
+            max[i] = (count[i] >= count[max[i+1]]) ? i : max[i+1];
+        }
+
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = max[starts[i]];
         }
         
         return answer;
