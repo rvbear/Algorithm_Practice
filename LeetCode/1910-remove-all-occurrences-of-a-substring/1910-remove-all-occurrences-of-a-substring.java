0ab34bffbcf8981814fa81884fda7059f3a36f71
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-11 16:43:13 +0900

    Time: 8 ms (30.12%), Space: 44.8 MB (26.43%) - LeetHub

---
+++ LeetCode/1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+
+        return s;
+    }
+}
