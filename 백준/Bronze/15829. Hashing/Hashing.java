d88bbe740210417bd098b353af91ea4376723976
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-12 09:59:55 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 14384 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,16 +4,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int L = Integer.parseInt(br.readLine());
-        char[] alphabet = br.readLine().toCharArray();
-        int ans = 0;
+        String s = br.readLine();
+        long ans = 0, pow = 1;
 
         for (int i = 0; i < L; i++) {
-            int n = alphabet[i] - 'a' + 1;
-            ans += (n * (int)Math.pow(31, i));
+            ans += (s.charAt(i) - 'a' + 1) * pow;
+            pow = (pow * 31) % 1234567891;
         }
 
-        System.out.println(ans);
+        System.out.println(ans % 1234567891);
     }
 }
