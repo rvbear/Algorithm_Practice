a92cdeaf2148625339318aa293f7a2ada7bbb503
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-25 09:08:19 +0900

    [Silver IV] Title: 듣보잡, Time: 228 ms, Memory: 24020 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -3,32 +3,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
 
-        int N = Integer.parseInt(stk.nextToken());      // 듣도 못한 사람 수
-        int M = Integer.parseInt(stk.nextToken());      // 보도 못한 사람 수
-
-        HashMap<String, String> hashMap = new HashMap<>();      // 듣도 못한 사람을 저장할 hashmap
-        ArrayList<String> arrayList = new ArrayList<>();        // 듣도 보도 못한 사람을 저장할 arrayList
-
-        for(int i = 0; i < N+M; i++) {
-            if(i < N) {                                         // i가 N보다 작을 경우 듣도 못한 사람들을 입력받아 hashmap에 저장
-                String listen = br.readLine();
-                hashMap.put(listen, listen);
-            }
-            else {                                              // i가 N보다 클 경우 보도 못한 사람들을 입력받아
-                String see = br.readLine();                     // hashmap에 있는지 비교한 뒤 있다면 배열에 저장
-                if(hashMap.containsKey(see))
-                    arrayList.add(see);
-            }
+        String[] notListen = new String[n];
+        for (int i = 0; i < n; i++) {
+            notListen[i] = br.readLine();
         }
 
-        Collections.sort(arrayList);                            // 배열을 정렬
-        System.out.println(arrayList.size());                   // 배열의 크기 출력
-        for(int i = 0; i < arrayList.size(); i++)               // 배열에 있는 값을 출력
-            System.out.println(arrayList.get(i));
+        String[] notSee = new String[m];
+        for (int i = 0; i < m; i++) {
+            notSee[i] = br.readLine();
+        }
 
-        br.close();     // 입력 버퍼 스트림 닫아줌
+        Arrays.sort(notListen);
+        Arrays.sort(notSee);
+
+        int nIdx = 0, mIdx = 0, res = 0;
+        while (nIdx < n && mIdx < m) {
+            int comp = notListen[nIdx].compareTo(notSee[mIdx]);
+
+            if (comp == 0) {
+                stb.append(notListen[nIdx]).append("\n");
+                res++;
+                nIdx++;
+                mIdx++;
+            } else if (comp > 0) {
+                mIdx++;
+            } else {
+                nIdx++;
+            }
+        }
+        
+        System.out.println(res);
+        System.out.println(stb.toString());
     }
 }
