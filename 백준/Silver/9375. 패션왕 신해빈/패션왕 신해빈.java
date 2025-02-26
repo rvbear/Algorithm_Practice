19327182df2a68983ef46f1c977b061d71a7e11f
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-02-26 11:30:34 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -4,28 +4,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int t = Integer.parseInt(br.readLine());                            // test case 개수
-        for(int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());                        // 옷의 개수
-            HashMap<String, Integer> map = new HashMap<>();                 // 값을 저장할 hashmap
-            for(int j = 0; j < n; j++) {
-                StringTokenizer stk = new StringTokenizer(br.readLine());
-                String a = stk.nextToken();                                 // 필요없는 값
-                String b = stk.nextToken();                                 // 실질적으로 필요한 값
-                if(map.containsKey(b)) {                                    // 해당 분류가 map에 있다면 값을 1 증가
-                    map.put(b, map.get(b) + 1);
-                } else {                                                    // 없다면 map에 넣어줌
-                    map.put(b, 1);
-                }
+        StringBuilder stb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (int i = 0; i < n; i++) {
+                String[] input = br.readLine().split(" ");
+                map.put(input[1], map.getOrDefault(input[1], 0) + 1);
             }
-            int result = 1;                                                 // 결과값
-            for(int s : map.values()) {                                     // 경우의 수를 구하면 됨
-                result *= (s + 1);                                          // 하나도 입지 않은 경우도 포함하여 곱해줌
+
+            int res = 1;
+            for (int num : map.values()) {
+                res *= num + 1;
             }
-            bw.write(String.valueOf(result - 1) + "\n");              // 하나도 입지 않은 경우가 모두 선택된 단 한가지의 경우를 제외해줌
+
+            stb.append(res - 1).append("\n");
         }
 
-        br.close(); bw.close();
+        System.out.println(stb.toString());
     }
 }
