ba69950778ffaf03b62fb6298e82031e4755e783
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-03 12:56:22 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14000 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -2,29 +2,29 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           // 버퍼 스트림을 통해 입력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));         // 버퍼 스트림을 통해 출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
 
-        int[][] room = new int[15][15];                     // 호실에 있는 사람 수
-        for(int i = 0; i < 15; i++)
-            for(int j = 1; j <= 14; j++) {
-                if(j == 1)                                  // i층 1호실일 경우
-                    room[i][j] = 1;
-                else if(i == 0)                             // 0층일 경우
-                    room[i][j] = j;
-                else                                        // 그 외의 경우
-                    for(int k = 1; k <= j; k++)
-                        room[i][j] += room[i-1][k];
-            }
-
-        int N = Integer.parseInt(br.readLine());            // Test Case 개수
-
-        for(int i = 0; i < N; i++) {
-            int a = Integer.parseInt(br.readLine());        // 층
-            int b = Integer.parseInt(br.readLine());        // 호수
-            bw.write(String.valueOf(room[a][b]) + "\n");
+        int[][] room = new int[15][15];
+        for (int i = 1; i < 15; i++) {
+            room[0][i] = i;
         }
 
-        br.close(); bw.close();                             // 입,출력 버퍼 스트림 닫아줌
+        for (int i = 1; i < 15; i++) {
+            room[i][1] = 1;
+            for (int j = 2; j < 15; j++) {
+                room[i][j] = room[i-1][j] + room[i][j-1];
+            }
+        }
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            stb.append(room[k][n]).append("\n");
+        }
+
+        System.out.println(stb.toString());
     }
 }
