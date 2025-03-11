facfc25e4887bf4d94b080f6bb260fb88a2830f2
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-11 09:17:26 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 412 ms, Memory: 39568 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -3,46 +3,50 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));     // 버퍼 스트림을 통해 출력
-        StringTokenizer stk;
-        int t = Integer.parseInt(br.readLine());            // testcase 수 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        String[] input;
 
-        for(int i = 0; i < t; i++) {
-            stk = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(stk.nextToken());      // A의 수
-            int m = Integer.parseInt(stk.nextToken());      // B의 수
-            int[] a = new int[n];
-            int[] b = new int[m];
+        while (t-- > 0) {
+            input = br.readLine().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            int[] arrA = new int[n];
+            int[] arrB = new int[m];
 
-            stk = new StringTokenizer(br.readLine());       // A의 값 입력
-            for(int j = 0; j < n; j++) {
-                a[j] = Integer.parseInt(stk.nextToken());
-            }
-            stk = new StringTokenizer(br.readLine());       // B의 값 입력
-            for(int j = 0; j < m; j++) {
-                b[j] = Integer.parseInt(stk.nextToken());
+            input = br.readLine().split(" ");
+            for(int i = 0; i < n; i++) {
+                arrA[i] = Integer.parseInt(input[i]);
             }
 
-            Arrays.sort(b);                                 // B 정렬
-            int count = 0;                                  // 쌍의 개수
+            input = br.readLine().split(" ");
+            for(int i = 0; i < m; i++) {
+                arrB[i] = Integer.parseInt(input[i]);
+            }
 
-            for(int j = 0; j < n; j++) {                    // 이진 탐색 시작
-                int start = 0, end = m-1, index = 0;
-                while(start <= end) {
-                    int mid = (start+end) / 2;
-                    if(b[mid] < a[j]) {                     // b 배열의 중간값이 원하는 값보다 작다면
-                        start = mid + 1;                    // 탐색 시작 지점을 중간값 + 1로 설정
-                    } else {                                // 아니라면
-                        end = mid - 1;                      // 탐색 종료 지점을 중간값 - 1로 설정
+            Arrays.sort(arrB);
+            int cnt = 0;
+
+            for (int i = 0; i < n; i++) {
+                int start = 0, end = m - 1, idx = 0;
+
+                while (start <= end) {
+                    int mid = (start + end) / 2;
+
+                    if (arrB[mid] < arrA[i]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
                     }
                 }
-                count += start;                             // start 값이 탐색이 끝났을 당시 쌍의 개수를 나타냄
+
+                cnt += start;
             }
-            bw.write(String.valueOf(count) + "\n");
+
+            stb.append(cnt).append("\n");
         }
 
-        br.close();                                         // 입력 버퍼 스트림 닫아줌
-        bw.close();                                         // 출력 버퍼 스트림 닫아줌
+        System.out.println(stb.toString());
     }
 }
