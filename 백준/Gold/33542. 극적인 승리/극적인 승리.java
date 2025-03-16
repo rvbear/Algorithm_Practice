fcc771d2d68243f7464a800c15d17315ca44a40b
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-16 14:09:01 +0900

    [Gold IV] Title: 극적인 승리, Time: 1436 ms, Memory: 163900 KB -BaekjoonHub

--- 백준/Gold/33542. 극적인 승리/극적인 승리.java
+++ 백준/Gold/33542. 극적인 승리/극적인 승리.java
@@ -1,78 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
-public class Main {
-    static int gap;
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
-    static int solve(int idx, int score, ArrayList<Pair> oth) {
-        if (gap < score)
-            return -1;
-        int s = -1, e = oth.size(), mid;
-        while (s + 1 < e) {
-            mid = (s + e) >> 1;
-            if (gap < score + oth.get(mid).score)
-                e = mid;
-            else
-                s = mid;
-        }
-        for (int i = e; i < oth.size(); i++)
-            if (idx != oth.get(i).idx)
-                return i;
-        return -2;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
-        gap = A - B;
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<Pair> L = new ArrayList<>(), R = new ArrayList<>();
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            L.add(new Pair(i, Integer.parseInt(st.nextToken())));
-            R.add(new Pair(i, Integer.parseInt(st.nextToken())));
-        }
-        Collections.sort(L);
-        Collections.sort(R);
-        int X = -2, Y = -2, min = Integer.MAX_VALUE;
-        if (gap < 0) {
-            X = Y = -1;
-            min = B;
-        }
-        for (int i = 0; i < N; i++) {
-            int idx = solve(L.get(i).idx, L.get(i).score, R);
-            if (idx == -2)
-                continue;
-            int score = B + L.get(i).score + (idx == -1 ? 0 : R.get(idx).score);
-            if (score < min) {
-                X = L.get(i).idx;
-                Y = idx == -1 ? -1 : R.get(idx).idx;
-                min = score;
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            int idx = solve(R.get(i).idx, R.get(i).score, L);
-            if (idx == -2)
-                continue;
-            int score = B + R.get(i).score + (idx == -1 ? 0 : L.get(idx).score);
-            if (score < min) {
-                X = idx == -1 ? -1 : L.get(idx).idx;
-                Y = R.get(i).idx;
-                min = score;
-            }
-        }
-        if (X == -2) {
-            System.out.println("No");
-            return;
-        }
-        System.out.printf("%d %d", X, Y);
-    }
-}
 
 class Pair implements Comparable<Pair> {
     int idx, score;
@@ -85,5 +12,94 @@ class Pair implements Comparable<Pair> {
     @Override
     public int compareTo(Pair o) {
         return this.score - o.score;
+    }
+}
+
+public class Main {
+    static int gap;
+    static StringTokenizer st;
+
+    static int binarySearch(int idx, int score, ArrayList<Pair> oth) {
+        if (gap < score) {
+            return -1;
+        }
+
+        int start = -1, end = oth.size(), mid;
+        while (start + 1 < end) {
+            mid = (start + end) >> 1;
+
+            if (gap < score + oth.get(mid).score) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+
+        for (int i = end; i < oth.size(); i++) {
+            if (idx != oth.get(i).idx) {
+                return i;
+            }
+        }
+
+        return -2;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+        gap = a - b;
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Pair> left = new ArrayList<>(), right = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            left.add(new Pair(i, Integer.parseInt(st.nextToken())));
+            right.add(new Pair(i, Integer.parseInt(st.nextToken())));
+        }
+
+        Collections.sort(left);
+        Collections.sort(right);
+
+        int x = -2, y = -2, min = Integer.MAX_VALUE;
+        if (gap < 0) {
+            x = y = -1;
+            min = b;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(left.get(i).idx, left.get(i).score, right);
+            if (idx == -2) {
+                continue;
+            }
+
+            int score = b + left.get(i).score + (idx == -1 ? 0 : right.get(idx).score);
+            if (score < min) {
+                x = left.get(i).idx;
+                y = idx == -1 ? -1 : right.get(idx).idx;
+                min = score;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int idx = binarySearch(right.get(i).idx, right.get(i).score, left);
+            if (idx == -2) {
+                continue;
+            }
+
+            int score = b + right.get(i).score + (idx == -1 ? 0 : left.get(idx).score);
+            if (score < min) {
+                x = idx == -1 ? -1 : left.get(idx).idx;
+                y = right.get(i).idx;
+                min = score;
+            }
+        }
+
+        if (x == -2) {
+            System.out.println("No");
+        } else {
+            System.out.printf(x + " " + y);
+        }
     }
 }
