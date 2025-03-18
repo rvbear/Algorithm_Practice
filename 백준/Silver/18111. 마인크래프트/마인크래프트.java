37506edd864a1ede19c20277622aa7a4bc056a02
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-18 21:13:46 +0900

    [Silver II] Title: 마인크래프트, Time: 356 ms, Memory: 38976 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -2,57 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
         int n = Integer.parseInt(input[0]);
         int m = Integer.parseInt(input[1]);
         int b = Integer.parseInt(input[2]);
 
-        int[][] map = new int[n][m];
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int[] heights = new int[257];
+        int mapSize = n * m;
+        int blocks = 0;
+        int minHeight = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             input = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(input[j]);
-                min = Math.min(min, map[i][j]);
-                max = Math.max(max, map[i][j]);
+                int h = Integer.parseInt(input[j]);
+                heights[h]++;
+                blocks += h;
+                minHeight = Math.min(h, minHeight);
             }
         }
 
-        int minTime = Integer.MAX_VALUE;
-        int maxHight = 0;
+        int maxHeight = b + blocks < (mapSize * 256) ? (b + blocks) / mapSize : 256;
+        int t = (blocks - (minHeight * mapSize)) * 2;
 
-        for (int h = min; h <= max; h++) {
-            int insCnt = 0;
-            int delCnt = 0;
-            int temp = b;
+        int time = t;
+        int height = minHeight;
+        blocks = 0;
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (map[i][j] < h) {
-                        insCnt += h - map[i][j];
-                        temp -= h - map[i][j];
-                    } else if (map[i][j] > h) {
-                        delCnt += 2 * (map[i][j] - h);
-                        temp += map[i][j] - h;
-                    }
-                }
-            }
+        for (int i = minHeight + 1; i <= maxHeight; i++) {
+            blocks += heights[i - 1];
+            t += blocks - ((mapSize - blocks) * 2);
 
-            if (temp < 0) {
-                continue;
-            }
-
-            int time = delCnt + insCnt;
-            if (time <= minTime && h >= maxHight) {
-                minTime = time;
-                maxHight = h;
+            if (time >= t) {
+                time = t;
+                height = i;
             }
         }
-        
-        System.out.println(minTime + " " + maxHight);
+
+        System.out.println(time + " " + height);
     }
 }
