e1d822bdf58c062d7c4d878cfbda1604898582b3
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-20 15:39:22 +0900

    [Silver II] Title: 외판원 순회 2, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,40 +1,55 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int n;
-    static int[][] map;
-    static boolean[] visit;
-    static int res = Integer.MAX_VALUE;
-    static void back(int start, int now, int cost, int count) {
-        if (map[now][start] != 0 && count == n) {
-            res = Math.min(res, cost+ map[now][start]);
-            return;
-        }
+public class Main {
+    static int n, INF = 100000000;
+    static int[][] map, dp;
 
-        for (int i = 0; i < n; i++){
-            if (map[now][i] > 0 && !visit[i]) {
-                visit[i] = true;
-                back(start, i,cost + map[now][i], count+1);
-                visit[i] = false;
+    static int tsp(int cur, int visited) {
+        if (visited == (1 << n) - 1) {
+            if (map[cur][0] == 0) {
+                return INF;
+            } else {
+                return map[cur][0];
             }
         }
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        visit = new boolean[n];
+
+        int result = dp[cur][visited];
+        if (result != -1) {
+            return result;
+        }
+
+        result = INF;
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            if (map[cur][i] != 0 && (visited & 1 << i) == 0) {
+                int temp = tsp(i, visited + (1 << i)) + map[cur][i];
+                result = Math.min(temp, result);
+            }
+        }
+
+        return dp[cur][visited] = result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new int[n][n];
+        dp = new int[n][1 << n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i],-1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visit[0] = true;
-        back(0,0,0,1);
-        System.out.println(res);
+        System.out.println(tsp(0,1));
     }
 }
