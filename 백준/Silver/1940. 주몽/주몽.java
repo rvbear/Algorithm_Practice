dbc87ff8c4840e5ec39e3e7567789128df0b0f77
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-25 09:08:31 +0900

    [Silver IV] Title: 주몽, Time: 156 ms, Memory: 16420 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -6,23 +6,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
-        int[] num = new int[n];
+        int[] numList = new int[n];
+        boolean[] visit = new boolean[100001];
+
         StringTokenizer stk = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            num[i] = Integer.parseInt(stk.nextToken());
+            numList[i] = Integer.parseInt(stk.nextToken());
+            visit[numList[i]] = true;
         }
 
-        Arrays.sort(num);
-        int start = 0, end = n-1, count = 0;
-        while (start != end) {
-            if(num[start] + num[end] <= m) {
-                count += num[start] + num[end] == m ? 1 : 0;
-                start++;
-                continue;
+        int cnt = 0;
+        for (int num : numList) {
+            int target = m - num;
+
+            if (-1 < target && target < 100001 && visit[target]) {
+                cnt++;
             }
-            end--;
         }
 
-        System.out.println(count);
+        System.out.println(cnt / 2);
     }
 }
