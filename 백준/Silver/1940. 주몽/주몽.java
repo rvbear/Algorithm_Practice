c2156241017c9193d175a0765c64d0c6c2f3261e
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-03-25 09:02:56 +0900

    [Silver IV] Title: 주몽, Time: 180 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -3,29 +3,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-        int n = Integer.parseInt(br.readLine());                    // 숫자의 개수
-        int m = Integer.parseInt(br.readLine());                    // 만들어야하는 숫자
-        int[] num = new int[n];                                     // 숫자를 저장할 배열
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {                                // 배열에 값을 저장
+        for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(stk.nextToken());
         }
 
-        Arrays.sort(num);                                           // 배열을 오름차순으로 정렬
-        int start = 0, end = n-1, count = 0;                        // 시작 포인터, 마지막 포인터, 만들어지는 개수 카운트
-        while(start != end) {                                       // 시작 포인터와 마지막 포인터가 다를 경우 종료
-            if(num[start] + num[end] <= m) {                        // 두 포인터가 가르키는 곳의 합이 m보다 작거나 같을 경우 중
-                if(num[start] + num[end] == m) {                    // 두 포인터가 가르키는 곳의 합이 m보다 같을 경우 count 증가
-                    count++;
-                }
-                start++;                                            // start 증가
-            } else {                                                // 두 포인터가 가르키는 곳의 합이 m보다 클 경우
-                end--;                                              // end 감소
+        Arrays.sort(num);
+        int start = 0, end = n-1, count = 0;
+        while (start != end) {
+            if(num[start] + num[end] <= m) {
+                count += num[start] + num[end] == m ? 1 : 0;
+                start++;
+                continue;
             }
+            end--;
         }
 
-        System.out.println(count);                                  // 결과값 출력
-        br.close();                                                 // 입력 버퍼 스트림 닫아줌
+        System.out.println(count);
     }
 }
