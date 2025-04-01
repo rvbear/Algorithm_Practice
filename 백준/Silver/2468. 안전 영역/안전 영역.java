b8f7f9e3e51e42b4b9a6d971f8ef09d2fd4a348f
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-01 09:12:55 +0900

    [Silver I] Title: 안전 영역, Time: 232 ms, Memory: 19340 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,13 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, max = 0;                              // 지역의 크기, 안전지대 개수
-    static ArrayList<Integer> list = new ArrayList<>(); // 비의 양을 저장할 배열
-    static int[] dx = {-1, 1, 0, 0};                    // 움직이는 x 좌표
-    static int[] dy = {0, 0, -1, 1};                    // 움직이는 y 좌표
-    static int[][] map;                                 // 지역의 정보 저장
-    static boolean[][] visit;                           // 방문여부
-    public static void dfs(int x, int y, int num) {     // dfs 탐색 시작
+    static int n, max = 0;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] map;
+    static boolean[][] visit;
+
+    public static void dfs(int x, int y, int num) {
         visit[x][y] = true;
 
         for(int i = 0; i < 4; i++) {
@@ -20,8 +21,9 @@ public class Main {
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
 
@@ -35,11 +37,11 @@ public class Main {
             }
         }
 
-        for(int num : list) {                               // 안전지대의 개수가 가장 많음을 비교해야하기 때문에 잠기는 높이를 반복문으로 비교
-            visit = new boolean[n][n];                      // 비교할 때마다 방문여부 배열 초기화
-            int count = 0;                                  // 안전지대 개수 초기화
+        for(int num : list) {
+            visit = new boolean[n][n];
+            int count = 0;
 
-            for(int i = 0; i < n; i++) {                    // 탐색 시작
+            for(int i = 0; i < n; i++) {
                 for(int j = 0; j < n; j++) {
                     if(!visit[i][j] && map[i][j] >= num) {
                         dfs(i, j, num);
@@ -48,10 +50,9 @@ public class Main {
                 }
             }
 
-            max = Math.max(count, max);                     // 안전지대의 개수 비교
+            max = Math.max(count, max);
         }
 
-        System.out.println(max);                            // 가장 많은 안전지대의 개수 출력
-        br.close();                                         // 입력 버퍼 스트림 닫아줌
+        System.out.println(max);
     }
 }
