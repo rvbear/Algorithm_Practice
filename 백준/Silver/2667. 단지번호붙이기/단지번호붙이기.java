cbae41d4bf0452acf4866a365a53036acaf30b19
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-03 12:45:43 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,54 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, count = 0, temp = 0;              // 크기, 단지의 개수, 단지의 크기
-    static int[][] map;                             // 단지 지도
-    static boolean[][] visit;                       // 방문 여부
-    static int[] dx = {-1, 1, 0, 0};                // 변화하는 x값
-    static int[] dy = {0, 0, -1, 1};                // 변화하는 y값
-    public static void dfs(int x, int y) {          // dfs를 통해 탐색
+    static int n, temp;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+
+    public static void dfs(int x, int y) {
         visit[x][y] = true;
-        for(int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             int mx = x + dx[i];
             int my = y + dy[i];
-            // 지도의 범위 안에 들면서 방문하지 않았지만 연속된 단지의 부분일 경우
-            if(mx >= 0 && my >= 0 && mx < n && my < n && !visit[mx][my] && map[mx][my] == 1) {
-                temp++;                             // 넓이 증가
+            if (mx >= 0 && my >= 0 && mx < n && my < n && !visit[mx][my] && map[mx][my] == 1) {
+                temp++;
                 dfs(mx, my);
             }
         }
     }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-        n = Integer.parseInt(br.readLine());    // 지도의 크기 입력받음
-        map = new int[n][n];                    // 배열 생성
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        temp = 0;
+        map = new int[n][n];
         visit = new boolean[n][n];
 
-        for(int i = 0; i < n; i++) {            // 배열에 해당 값 넣어줌
+        for (int i = 0; i < n; i++) {
             String line = br.readLine();
-            for(int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        ArrayList<Integer> size = new ArrayList<>();        // 각 단지의 크기를 저장할 배열
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {                    // 탐색 시작
-                if(!visit[i][j] && map[i][j] == 1) {
+        ArrayList<Integer> size = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j] && map[i][j] == 1) {
                     temp = 1;
-                    count++;
                     dfs(i, j);
                     size.add(temp);
                 }
             }
         }
 
-        Collections.sort(size);                             // 오름차순으로 정렬
+        Collections.sort(size);
 
-        System.out.println(count);                          // 단지의 개수 출력
-        for(int num : size) {                               // 정렬된 순서대로 각 단지의 크기 출력
-            System.out.println(num);
+        stb.append(size.size()).append("\n");
+        for (int num : size) {
+            stb.append(num).append("\n");
         }
-        br.close();                                         // 입력 버퍼 스트림 닫아줌
+
+        System.out.print(stb.toString());
     }
 }
