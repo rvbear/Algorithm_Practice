4e56f1c9db99e21e34774f6fb065d73671d2d304
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-07 11:18:46 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14480 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,28 +1,28 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           // 버퍼 스트림을 통해 입력
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] numbers = new long[N+1];                             // 수열을 저장할 배열
-        long[] DP = new long[N+1];                                  // DP 배열
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++) {                               // 수열의 값을 저장
-            numbers[i] = Long.parseLong(stk.nextToken());
-            DP[i] = 1;                                              // 초깃값 설정
+        long[] numbers = new long[N + 1];
+        long[] DP = new long[N + 1];
+        String[] input = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            numbers[i] = Long.parseLong(input[i - 1]);
+            DP[i] = 1;
         }
 
         long max = 1;
-        for(int i = 1; i <= N; i++) {
-            for(int j = 1; j < i; j++)
-                if(numbers[i] > numbers[j])                         // 이전 값들 중 가장 큰 DP값 + 1
-                    DP[i] = Math.max(DP[i], DP[j]+1);
-            max = Math.max(max, DP[i]);                             // LIS 길이 구하기
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (numbers[i] > numbers[j]) {
+                    DP[i] = Math.max(DP[i], DP[j] + 1);
+                }
+            }
+            
+            max = Math.max(max, DP[i]);
         }
 
-        System.out.println(max);                                    // 결과값 출력
-        br.close();                                                 // 입력 버퍼 스트림 닫아줌
+        System.out.println(max);
     }
 }
