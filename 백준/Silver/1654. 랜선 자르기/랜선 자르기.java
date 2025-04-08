bde27808772c012fd2787e2da5cd3f7cb9d5079d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-08 09:20:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 148 ms, Memory: 17320 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,35 +1,38 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));           // 버퍼 스트림을 통해 입력
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
 
-        int K = Integer.parseInt(stk.nextToken());          // 이미 가지고 있는 랜선의 개수
-        int N = Integer.parseInt(stk.nextToken());          // 필요한 랜선의 개수
-        long[] lines = new long[K];                         // 랜선의 길이를 저장할 배열
-        long max = 0;                                       // 가장 긴 랜선의 길이
+        int k = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+        long[] lines = new long[k];
+        long max = 0;
 
-        for(int i = 0; i < K; i++) {
+        for (int i = 0; i < k; i++) {
             lines[i] = Long.parseLong(br.readLine());
-            if(max < lines[i]) max = lines[i];
+            if (max < lines[i]) {
+                max = lines[i];
+            }
         }
 
-        long min = 1, mid = 0, count;                       // 최솟값 = 1로 설정, 중간값, count 개수 선언
+        long min = 1;
+        while (min <= max) {
+            long mid = (min + max) / 2;
+            long cnt = 0;
 
-        while(min <= max) {
-            count = 0;                                      // count 초깃값 설정
+            for (int i = 0; i < k; i++) {
+                cnt += lines[i] / mid;
+            }
 
-            mid = (max + min) / 2;                          // 중간값 설정
-            for(int i = 0; i < K; i++)                      // 중간값을 통해 나오는 랜선의 개수를 count에 저장
-                count += lines[i] / mid;
-
-            if(count < N) max = mid - 1;                    // count가 N보다 작을 경우 더 줄여야함
-            else min = mid + 1;                             // count가 N보다 클 경우 더 늘릴 수 있음
+            if (cnt < n) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
         }
 
-        System.out.println((max + min) / 2);                // 잘라야하는 랜선의 길이
-        br.close();                                         // 입력 버퍼 스트림 닫아줌
+        System.out.println((min + max) / 2);
     }
 }
