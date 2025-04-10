6086e63c0122d621ae6fc68e6ea925a760e3b13b
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-10 16:11:12 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,32 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-        StringTokenizer stk;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());            // Test case 개수
-        int[] T = new int[N];
-        int[] P = new int[N];
-        int[] DP = new int[N+1];
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n];
+        int[] p = new int[n];
+        int[] dp = new int[n + 1];
 
-        for(int i = 0; i < N; i++) {
-            stk = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(stk.nextToken());     // 상담시간
-            P[i] = Integer.parseInt(stk.nextToken());     // 수익
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            t[i] = Integer.parseInt(input[0]);
+            p[i] = Integer.parseInt(input[1]);
         }
 
-        for(int i = N-1; i >= 0; i--) {
-            int period = i + T[i];                        // 상담기간
+        for (int i = n - 1; i >= 0; i--) {
+            int period = i + t[i];
 
-            if(period <= N)                               // 상담이 가능할 경우
-                DP[i] = Math.max(P[i] + DP[period], DP[i+1]);
-            else                                          // 상담이 불가능할 경우
-                DP[i] = DP[i+1];
+            if (period <= n) {
+                dp[i] = Math.max(p[i] + dp[period], dp[i + 1]);
+            } else {
+                dp[i] = dp[i + 1];
+            }
         }
 
-        System.out.println(DP[0]);                        // 최댓값 출력
-        br.close();         // 입력 버퍼 스트림 닫아줌
+        System.out.println(dp[0]);
     }
 }
