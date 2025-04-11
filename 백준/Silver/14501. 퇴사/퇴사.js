56e0af1f2fb35f88c4867ab94435767acb7a3c84
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-11 10:03:18 +0900

    [Silver III] Title: 퇴사, Time: 96 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.js
+++ 백준/Silver/14501. 퇴사/퇴사.js
@@ -4,12 +4,12 @@ let [n, ...input] = require("fs")
   .trim()
   .split("\n");
 
-n = Number(n);
+n = +n;
 input = input.map((v) => v.split(" ").map(Number));
 let dp = Array(n + 1).fill(0);
 
 for (let i = n - 1; i >= 0; i--) {
-  let temp = i + Number(input[i][0]);
+  let temp = i + +input[i][0];
 
   if (temp <= n) {
     dp[i] = Math.max(input[i][1] + dp[temp], dp[i + 1]);
