8e30914a933c1ec229f02ceacedafbd6aa5363f1
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-14 10:08:40 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -2,16 +2,16 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Integer N = Integer.parseInt(br.readLine());        // 자릿수 입력
-        long[] DP = new long[N + 1];
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n + 1];
 
-        DP[1] = 1;
-        for(int i = 2; i <= N; i++)                     // 이친수의 개수는 피보나치 수열
-            DP[i] = DP[i - 2] + DP[i - 1];
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
 
-        System.out.println(DP[N]);                      // 원하는 값 출력
-        br.close();         // 입력 버퍼 스트림 닫아줌
+        System.out.println(dp[n]);
     }
 }
