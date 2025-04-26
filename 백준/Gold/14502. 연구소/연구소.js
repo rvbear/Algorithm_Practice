886bdd06e2f04a1ecf8d807df226a4d6a905fdc2
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-04-26 19:43:24 +0900

    [Gold IV] Title: 연구소, Time: 408 ms, Memory: 13932 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.js
+++ 백준/Gold/14502. 연구소/연구소.js
@@ -5,57 +5,72 @@ let input = require("fs")
   .split("\n");
 
 let [n, m] = input[0].split(" ").map(Number);
-let map = input.slice(1).map((line) => line.split(" ").map(Number));
-let max = 0;
+let map = [], temp = [];
+
+for (let i = 1; i < n + 1; i++) {
+  map.push(input[i].split(" ").map(Number));
+  temp.push(new Array(m).fill(0));
+}
 
 let dir = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+let max = 0;
 
-const spread = (temp) => {
-  let q = [];
+const spread = (x, y) => {
+  for (let i = 0; i < 4; i++) {
+    let nx = x + dir[i][0];
+    let ny = y + dir[i][1];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (temp[i][j] === 2) {
-        q.push([i, j]);
-      }
+    if (nx < 0 || ny < 0 || nx > n - 1 || ny > m - 1) {
+      continue;
     }
-  }
 
-  while (q.length) {
-    let [x, y] = q.pop();
-    for (let [dx, dy] of dir) {
-      let nx = x + dx;
-      let ny = y + dy;
-
-      if (nx >= 0 && nx < n && ny >= 0 && ny < m && temp[nx][ny] === 0) {
-        temp[nx][ny] = 2;
-        q.push([nx, ny]);
-      }
+    if (temp[nx][ny] === 0) {
+      temp[nx][ny] = 2;
+      spread(nx, ny);
     }
   }
 };
 
-const getCount = (temp) => temp.flat().filter((cell) => cell === 0).length;
+const getCount = () => {
+  let cnt = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (temp[i][j] === 0) {
+        cnt += 1;
+      }
+    }
+  }
 
-const dfs = (depth) => {
+  return cnt;
+};
+
+const dfs = (x, y, depth) => {
   if (depth === 3) {
-    let temp = map.map((row) => row.slice());
-    spread(temp);
-    max = Math.max(max, getCount(temp));
+    temp = map.map(row => row.slice());
+
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (temp[i][j] === 2) {
+          spread(i, j);
+        }
+      }
+    }
+
+    max = Math.max(max, getCount());
     return;
   }
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
+  for (let i = x; i < n; i++) {
+    for (let j = i === x ? y : 0; j < m; j++) {
       if (map[i][j] === 0) {
         map[i][j] = 1;
-        dfs(depth + 1);
+        dfs(i, j + 1, depth + 1);
         map[i][j] = 0;
       }
     }
   }
 };
 
-dfs(0);
+dfs(0, 0, 0);
 
 console.log(max);
