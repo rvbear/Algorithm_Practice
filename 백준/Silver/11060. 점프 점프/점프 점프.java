b3ebe711af59f78848634982ea8c418139cbc1ca
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-05-12 23:19:59 +0900

    [Silver II] Title: 점프 점프, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,28 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());        // 입력 개수
-        int[] num = new int[n+1]; long[] dp = new long[1101];
-
-        StringTokenizer stk = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++) {                   // 초기화
-            num[i] = Integer.parseInt(stk.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n + 1];
+        long[] dp = new long[1101];
+        String[] input = br.readLine().split(" ");
+        for (int i = 1; i <= n; i++) {
+            num[i] = Integer.parseInt(input[i - 1]);
             dp[i] = Integer.MAX_VALUE;
         }
 
-        dp[1] = 0;                                      // 초깃값 설정
-        for(int i = 1; i <= n; i++) {                   // dp를 통해 최솟값 구하기
-            if(dp[i] >= Integer.MAX_VALUE) continue;
-            for (int j = 1; j <= num[i]; j++)
+        dp[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dp[i] >= Integer.MAX_VALUE) {
+                continue;
+            }
+
+            for (int j = 1; j <= num[i]; j++) {
                 dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+            }
         }
 
-        if(dp[n] >= Integer.MAX_VALUE) System.out.println(-1);
-        else System.out.println(dp[n]);
-        br.close();                                     // 입력 버퍼 스트림 닫아줌
+        System.out.println(dp[n] >= Integer.MAX_VALUE ? -1 : dp[n]);
     }
 }
