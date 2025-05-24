b065a913c90540cedcc872cdf9af9c477ea0a66c
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-05-24 21:47:49 +0900

    [Gold IV] Title: 리모컨, Time: 116 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,37 +1,37 @@
 import java.io.*;
 
 public class Main {
+    static int n, m, ans;
+    static boolean[] check;
+
+    static void dfs(int num, int depth) {
+        if (depth == 6) {
+            return;
+        }
+        
+        num *= 10;
+        for (int i = 0; i < 10; i++) {
+            if (!check[i]) {
+                ans = Math.min(ans, Math.abs(n - (num + i)) + depth + 1);
+                dfs(num + i, depth + 1);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-        boolean[] broken = new boolean[10];
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        check = new boolean[10];
         if (m != 0) {
             String[] input = br.readLine().split(" ");
             for (int i = 0; i < m; i++) {
-                broken[Integer.parseInt(input[i])] = true;
+                check[Integer.parseInt(input[i])] = true;
             }
         }
 
-        int res = Math.abs(n - 100);
-
-        for (int i = 0; i < 1000000; i++) {
-            String str = String.valueOf(i);
-            int len = str.length();
-
-            boolean flag = false;
-            for (int j = 0; j < len; j++) {
-                if (broken[str.charAt(j) - '0']) {
-                    flag = true;
-                    break;
-                }
-            }
-
-            if (!flag) {
-                res = Math.min(res, len + Math.abs(n - i));
-            }
-        }
-
-        System.out.println(res);
+        ans = Math.abs(n - 100);
+        dfs(0, 0);
+        System.out.println(ans);
     }
 }
