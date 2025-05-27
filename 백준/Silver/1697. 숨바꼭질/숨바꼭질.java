d13e0dffadd7f03693a626538be22f948aaf6bbb
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-05-27 20:23:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 120 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,51 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int n, k;
-    static int[] check = new int[100001];
-
-    public static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        check[start] = 1;
-
-        while (!queue.isEmpty()) {
-            int temp = queue.poll();
-
-            for (int i = 0; i < 3; i++) {
-                int next;
-                if (i == 0) {
-                    next = temp + 1;
-                } else if (i == 1) {
-                    next = temp - 1;
-                } else {
-                    next = temp * 2;
-                }
-
-                if (next == k) {
-                    System.out.println(check[temp]);
-                    return;
-                }
-
-                if (next >= 0 && next < check.length && check[next] == 0) {
-                    queue.add(next);
-                    check[next] = check[temp] + 1;
-                }
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        k = Integer.parseInt(input[1]);
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] dp = new int[100100];
 
-        if (n == k) {
-            System.out.println(0);
-        } else {
-            bfs(n);
+        for (int i = 0; i < n; i++) {
+            dp[i] = n - i;
         }
+
+        for (int i = n + 1; i <= k; i++) {
+            int min;
+
+            if (i % 2 == 0) {
+                min = dp[i / 2] + 1;
+            } else {
+                min = Math.min(dp[(i + 1) / 2], dp[(i - 1) / 2]) + 2;
+            }
+
+            dp[i] = Math.min(min, dp[i - 1] + 1);
+        }
+
+        System.out.println(dp[k]);
     }
 }
