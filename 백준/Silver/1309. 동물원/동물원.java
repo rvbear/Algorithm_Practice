eb836db873c54be52a94ca28ae5e5949ecdf9486
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-06-05 20:49:01 +0900

    [Silver I] Title: 동물원, Time: 108 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,19 +1,17 @@
 import java.io.*;
 
 public class Main {
-    static int mod = 9901;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       // 버퍼 스트림을 통해 입력
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long[] dp = new long[n+1];
-        dp[0] = 1; dp[1] = 3;                           // 초깃값 설정
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 3;
 
-        for(int i = 2; i <= n; i++) {                   
-            dp[i] = (dp[i-1] * 2 + dp[i-2]) % mod;      // 점화식  
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
         }
 
         System.out.println(dp[n]);
-        br.close();                                     // 입력 버퍼 스트림 닫아줌
     }
 }
