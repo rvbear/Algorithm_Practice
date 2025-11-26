0625066e807045affe879916e6693b16d30d6727
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2025-11-26 13:44:46 +0900

    [level 3] Title: 봉인된 주문, Time: 595.91 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/3/389481. 봉인된 주문/봉인된 주문.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
+    public long strToLong(String str) {
+        int len = str.length();
+        long num = 0;
+        
+        for (int i = 0; i < len; i++) {
+            int idx = 0;
+            
+            for (int j = 0; j < alphabet.length(); j++) {
+                if (str.charAt(i) == alphabet.charAt(j)) {
+                    idx = j + 1;
+                    break;
+                }
+            }
+            
+            if (i == len - 1) {
+                num += idx;
+            } else {
+                num += Math.pow(26, len - i - 1) * idx;
+            }
+        }
+        
+        return num - 1;
+    }
+    
+    public String longToStr(long num) {
+        if (num >= 0 && num < 26) {
+            return String.valueOf(alphabet.charAt((int)num));
+        } else {
+            return longToStr(num / 26 - 1) + longToStr(num % 26);
+        }
+    }
+    
+    public String solution(long n, String[] bans) {
+        Arrays.sort(bans, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length() - o2.length();
+            }
+        });
+        
+        n--;
+        
+        for (int i = 0; i < bans.length; i++) {
+            long num = strToLong(bans[i]);
+            
+            if (num <= n) {
+                n++;
+            }
+        }
+        
+        return longToStr(n);
+    }
+}
