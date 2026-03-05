166602e3795e802c0b50c96e83819689697b1148
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-05 20:48:14 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int minOperations(String s) {
-        int n = s.length(), c = 0, j = 0;
+    private static int counter(String s, char start) {
+        int cnt = 0;
 
-        for (char ch : s.toCharArray()) {
-            if (ch - '0' == j) {
-                c++;
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0 && s.charAt(i) != start) {
+                cnt++;
+            } else if (i % 2 == 1 && s.charAt(i) == start) {
+                cnt++;
             }
-
-            j ^= 1;
         }
 
-        return Math.min(c, n - c);
+        return cnt;
+    }
+
+    public int minOperations(String s) {
+        return Math.min(counter(s, '0'), counter(s, '1'));
     }
 }
