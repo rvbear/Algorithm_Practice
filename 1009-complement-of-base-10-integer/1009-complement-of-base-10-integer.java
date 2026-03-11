94582f67dc8c403c5d7038e5365752bf4b2eb7a3
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-11 21:38:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -1,11 +1,6 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        int mask = n | 1;
-
-        for (int i = 0; i <= 4; i++) {
-            mask |= mask >> (1 << i);
-        }
-
+        int mask = (Integer.highestOneBit(n) << 1) - 1;
         return n ^ mask;
     }
 }
