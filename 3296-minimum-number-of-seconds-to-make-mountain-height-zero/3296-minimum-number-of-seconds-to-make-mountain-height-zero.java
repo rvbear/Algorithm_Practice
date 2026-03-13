e060e06316f71237e2f3ec5ac287f71483db9e86
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-13 21:55:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,22 +1,35 @@
 class Solution {
-    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-        long low = 1, high = 10_000_000_000_000_000L;
-
-        while (low < high) {
-            long mid = (low + high) >> 1;
-            long temp = 0;
-
-            for (int i = 0; i < workerTimes.length && temp < mountainHeight; i++) {
-                temp += (long) (Math.sqrt((double) mid / workerTimes[i] * 2 + 0.25) - 0.5);
-            }
-
-            if (temp >= mountainHeight) {
-                high = mid;
-            } else {
-                low = mid + 1;
+    boolean check(int mountainHeight, int[] workerTimes, long time) {
+        for (int x : workerTimes) {
+            mountainHeight -= (int)(-1 + Math.sqrt(1 + 8 * time / x)) / 2;
+            
+            if (mountainHeight <= 0) {
+                return true;
             }
         }
 
-        return low;
+        return false;
+    }
+
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        int max = 0;
+        for (int x : workerTimes) {
+            max = Math.max(max, x);
+        }
+
+        int h = (mountainHeight-1) / workerTimes.length + 1;
+        long left = 1, right = (long) max * h * (h + 1) / 2;
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            
+            if (check(mountainHeight, workerTimes, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
