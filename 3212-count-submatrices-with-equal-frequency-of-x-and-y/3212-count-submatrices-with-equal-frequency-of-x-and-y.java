2644b831ebd4300be9919034ec6a9e3b23d9b65b
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-19 18:03:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,29 +1,33 @@
 class Solution {
     public int numberOfSubmatrices(char[][] grid) {
         int n = grid.length, m = grid[0].length;
-        int[][] diff = new int[n + 1][m + 1];
-        boolean[][] hasX = new boolean[n + 1][m + 1];
-        int cnt = 0;
+        int[] colX = new int[m];
+        int[] colY = new int[m];
+        int ans = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                int val = 0;
-
-                if (grid[i - 1][j - 1] == 'X') {
-                    val = 1;
-                } else if (grid[i - 1][j - 1] == 'Y') {
-                    val = -1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 'X') {
+                    colX[j]++;
                 }
 
-                diff[i][j] = diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1] + val;
-                hasX[i][j] = hasX[i - 1][j] || hasX[i][j - 1] || hasX[i - 1][j - 1] || (grid[i - 1][j - 1] == 'X');
+                if(grid[i][j] == 'Y') {
+                    colY[j]++;
+                }
+            }
+            
+            int x = 0, y = 0;
 
-                if (hasX[i][j] && diff[i][j] == 0) {
-                    cnt++;
+            for(int j = 0; j < m; j++) {
+                x += colX[j];
+                y += colY[j];
+
+                if(x == y && x > 0) {
+                    ans++;
                 }
             }
         }
 
-        return cnt;
+        return ans;
     }
 }
