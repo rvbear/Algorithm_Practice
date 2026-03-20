8f1d8a52b95410547cc0cec2730ea9934d438e67
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-03-20 16:34:53 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.java
@@ -1,32 +1,34 @@
 class Solution {
+    public int getmn(int[] arr){
+        Arrays.sort(arr);
+        int mn = Integer.MAX_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] != arr[i]) {
+                mn = Math.min(mn, Math.abs(arr[i] - arr[i - 1]));
+            }
+        }
+
+        return mn == Integer.MAX_VALUE ? 0 : mn;
+    }
+
     public int[][] minAbsDiff(int[][] grid, int k) {
         int n = grid.length, m = grid[0].length;
         int[][] ans = new int[n - k + 1][m - k + 1];
 
-        for (int i = 0; i <= n - k; i++) {
-            for (int j = 0; j <= m - k; j++) {
-                List<Integer> v = new ArrayList<>();
-
-                for (int x = i; x < i + k; x++) {
-                    for (int y = j; y < j + k; y++) {
-                        v.add(grid[x][y]);
+        for (int row = 0; row < n - k + 1; row++) {
+            for (int col = 0; col < m - k + 1; col++) {
+                int[] arr = new int[k * k];
+                int idx = 0;
+                
+                for (int i = row; i < row + k; i++) {
+                    for (int j = col; j < col + k; j++) {
+                        arr[idx] = grid[i][j];
+                        idx++;
                     }
                 }
 
-                Collections.sort(v);
-
-                int val = Integer.MAX_VALUE;
-                int prev = v.get(0);
-
-                for (int p = 1; p < v.size(); p++) {
-                    if (v.get(p) != prev) {
-                        val = Math.min(val, v.get(p) - prev);
-                    }
-
-                    prev = v.get(p);
-                }
-
-                ans[i][j] = (val == Integer.MAX_VALUE) ? 0 : val;
+                ans[row][col] = getmn(arr);
             }
         }
 
