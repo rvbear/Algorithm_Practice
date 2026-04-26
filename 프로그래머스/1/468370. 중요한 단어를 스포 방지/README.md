abf5656490c34cc5c9633a2605673b0fba66d0ab
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-04-26 22:56:37 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 2.30 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/README.md
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/README.md
@@ -1,10 +1,10 @@
 # [level 1] 중요한 단어를 스포 방지 - 468370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468370?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468370) 
 
 ### 성능 요약
 
-메모리: 97.1 MB, 시간: 4.86 ms
+메모리: 66.8 MB, 시간: 2.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 26일 22:48:51
+2026년 04월 26일 22:56:35
 
 ### 문제 설명
 
--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -2,37 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(String message, int[][] spoiler_ranges) {
-        List<String> spoilerWords = new ArrayList<>();
-        Set<String> noSpoilerWords = new HashSet<>();
-        int curIndex = 0;
+        int answer = 0;
+        Set<String> set = new HashSet<>();
+        StringBuilder blind = new StringBuilder(message);
         
-        for (String word : message.split(" ")) {
-            int start = message.indexOf(word, curIndex);
-            int end = start + word.length() - 1;
-            curIndex = end + 1;
-            boolean isSpoiler = false;
-            
-            for (int[] range : spoiler_ranges) {
-                if (start <= range[1] && range[0] <= end) {
-                    spoilerWords.add(word);
-                    isSpoiler = true;
-                    break;
+        for (int[] range : spoiler_ranges) {
+            for (int i = range[0]; i <= range[1]; i++) {
+                if (blind.charAt(i) != ' ') {
+                    blind.setCharAt(i, '*');
                 }
             }
-            
-            if (!isSpoiler) {
-                noSpoilerWords.add(word);
+        }
+        
+        for (String word : blind.toString().split(" ")) {
+            set.add(word);
+        }
+        
+        for (String word : message.split(" ")) {
+            if (!set.contains(word)) {
+                answer++;
+                set.add(word);
             }
         }
         
-        Set<String> importantWords = new HashSet<>();
-        
-        for (String spoilerWord : spoilerWords) {
-            if (!importantWords.contains(spoilerWord) && !noSpoilerWords.contains(spoilerWord)) {
-                importantWords.add(spoilerWord);
-            }
-        }
-        
-        return importantWords.size();
+        return answer;
     }
 }
