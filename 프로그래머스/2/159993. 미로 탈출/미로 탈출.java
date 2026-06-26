88e91493dcbad0182729ec58a8c32229d137365d
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-26 10:09:15 +0900

    [level 2] Title: 미로 탈출, Time: 5.51 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -4,13 +4,14 @@ class Solution {
     char[][] map;
     int[] dx = {-1, 0, 1, 0};
     int[] dy = {0, -1, 0, 1};
+    
     public int bfs(int x, int y, char t) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x, y, 0});
         
         boolean[][] visit = new boolean[map.length][map[0].length];
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] temp = q.poll();
             int nx = temp[0];
             int ny = temp[1];
@@ -26,28 +27,30 @@ class Solution {
                 int mx = nx + dx[i];
                 int my = ny + dy[i];
                 
-                if(mx >= 0 && my >= 0 && mx < map.length && my < map[0].length 
-                   && !visit[mx][my] && map[mx][my] != 'X') {
+                if (mx >= 0 && my >= 0 
+                    && mx < map.length && my < map[0].length 
+                    && !visit[mx][my] && map[mx][my] != 'X') {
                     visit[mx][my] = true;
-                    q.add(new int[]{mx, my, count + 1});
+                    q.add(new int[] {mx, my, count + 1});
                 }
             }
         }
         
         return -1;
     }
+    
     public int solution(String[] maps) {
         map = new char[maps.length][maps[0].length()];
         int sx = 0, sy = 0, lx = 0, ly = 0;
         
-        for(int i = 0; i < maps.length; i++) {
-            for(int j = 0; j < maps[0].length(); j++) {
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[0].length(); j++) {
                 map[i][j] = maps[i].charAt(j);
                 
-                if(map[i][j] == 'L') {
+                if (map[i][j] == 'L') {
                     lx = i;
                     ly = j;
-                } else if(map[i][j] == 'S') {
+                } else if (map[i][j] == 'S') {
                     sx = i;
                     sy = j;
                 }
