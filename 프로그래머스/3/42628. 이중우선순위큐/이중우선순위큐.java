612cfec3c1181718d026c32f1a3a96aa3c6f2d5e
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-27 22:59:44 +0900

    [level 3] Title: 이중우선순위큐, Time: 40.55 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -6,29 +6,29 @@ class Solution {
         PriorityQueue<Integer> min = new PriorityQueue<>();
         PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
         
-        for(String op : operations) {
+        for (String op : operations) {
             String[] order = op.split(" ");
             
-            if(order[0].equals("D") && min.size() < 1) {
+            if (order[0].equals("D") && min.size() < 1) {
                 continue;
             }
             
             int value = Integer.parseInt(order[1]);
             
-            if(order[0].equals("I")) {
+            if (order[0].equals("I")) {
                 min.offer(value);
                 max.offer(value);
                 continue;
             }
             
-            if(order[0].equals("D") && value == -1) {
+            if (order[0].equals("D") && value == -1) {
                 max.remove(min.poll());
-            } else if(order[0].equals("D") && value == 1) {
+            } else if (order[0].equals("D") && value == 1) {
                 min.remove(max.poll());
             }
         }
         
-        if(min.size() > 0) {
+        if (min.size() > 0) {
             answer[0] = max.poll();
             answer[1] = min.poll();
         }
