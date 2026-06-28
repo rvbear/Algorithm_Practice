3e0bbbb07ce2878051e92f18474b5019d15eb8c1
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-28 18:44:24 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,22 +1,24 @@
 class Solution {
     static boolean[] visit;
+    
     public void dfs(int[][] computers, int i) {
         visit[i] = true;
         
-        for(int j = 0; j < computers.length; j++) {
-            if(i != j && computers[i][j] == 1 && !visit[j]) {
+        for (int j = 0; j < computers.length; j++) {
+            if (i != j && computers[i][j] == 1 && !visit[j]) {
                 dfs(computers, j);
             }
         }
         
         return;
     }
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visit = new boolean[n];
         
-        for(int i = 0; i < n; i++) {
-            if(!visit[i]) {
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
                 dfs(computers, i);
                 answer++;
             }
