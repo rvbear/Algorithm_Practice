d019923bbeb4a74f84ea64314fcd21f5add636a5
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-06-30 10:10:41 +0900

    [level 3] Title: 베스트앨범, Time: 4.08 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -6,9 +6,10 @@ class Solution {
         Map<String, Integer> num = new HashMap<>();
         Map<String, HashMap<Integer, Integer>> music = new HashMap<>();
         
-        for(int i = 0; i < plays.length; i++) {
-            if(!num.containsKey(genres[i])) {
+        for (int i = 0; i < plays.length; i++) {
+            if (!num.containsKey(genres[i])) {
                 HashMap<Integer, Integer> map = new HashMap<>();
+                
                 map.put(i, plays[i]);
                 music.put(genres[i], map);
                 num.put(genres[i], plays[i]);
@@ -21,14 +22,15 @@ class Solution {
         List<String> keySet = new ArrayList(num.keySet());
         Collections.sort(keySet, (s1, s2) -> num.get(s2) - num.get(s1));
         
-        for(String key : keySet) {
+        for (String key : keySet) {
             Map<Integer, Integer> map = music.get(key);
             List<Integer> genresKey = new ArrayList(map.keySet());
             
             Collections.sort(genresKey, (s1, s2) -> map.get(s2) - map.get(s1));
             
             answer.add(genresKey.get(0));
-            if(genresKey.size() > 1) {
+            
+            if (genresKey.size() > 1) {
                 answer.add(genresKey.get(1));
             }
         }
