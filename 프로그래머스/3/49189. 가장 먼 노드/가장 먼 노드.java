06372af5ee8baea860d67c008aea4e5f24989814
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-01 10:03:04 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.14 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -4,48 +4,51 @@ class Solution {
     ArrayList<Integer>[] node;
     int[] visit;
     int depth = 0;
-    public void bfs(int start, int count) {
+    
+    private void bfs(int start, int count) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {start, count});
+        
+        q.add(new int[] { start, count });
         visit[start] = count;
         
-        while(!q.isEmpty()) {
+        while (!q.isEmpty()) {
             int[] temp = q.poll();
             
-            if(depth < temp[1]) {
+            if (depth < temp[1]) {
                 depth = temp[1];
             }
             
-            for(int i = 0; i < node[temp[0]].size(); i++) {
+            for (int i = 0; i < node[temp[0]].size(); i++) {
                 int next = node[temp[0]].get(i);
                 
-                if(visit[next] != 0) {
+                if (visit[next] != 0) {
                     continue;
                 }
                 
                 visit[next] = temp[1] + 1;
-                q.add(new int[]{next, visit[next]});
+                q.add(new int[] { next, visit[next] });
             }
         }
     }
+    
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        visit = new int[n+1];
-        node = new ArrayList[n+1];
+        visit = new int[n + 1];
+        node = new ArrayList[n + 1];
         
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             node[i] = new ArrayList<>();
         }
         
-        for(int i = 0; i < edge.length; i++) {
+        for (int i = 0; i < edge.length; i++) {
             node[edge[i][0]].add(edge[i][1]);
             node[edge[i][1]].add(edge[i][0]);
         }
         
         bfs(1, 1);
         
-        for(int i = 1; i <= n; i++) {
-            if(depth == visit[i]) {
+        for (int i = 1; i <= n; i++) {
+            if (depth == visit[i]) {
                 answer++;
             }
         }
