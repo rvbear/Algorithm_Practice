bc3e886aa14c8e78683de7ea982352b4961e694c
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-08 10:02:59 +0900

    [level 3] Title: 풍선 터트리기, Time: 26.02 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,25 +1,27 @@
 class Solution {
     public int solution(int[] a) {
-        int[] leftMin = new int[a.length];
-        int[] rightMin = new int[a.length];
-        int left = a[0], right = a[a.length - 1], answer = 2;
+        int n = a.length;
+        int[] leftMin = new int[n];
+        int[] rightMin = new int[n];
+        int left = a[0], right = a[n - 1], answer = 2;
         
-        if(a.length == 1) {
+        if (n == 1) {
             return 1;
         }
         
-        for(int i = 1; i < a.length - 1; i++) {
+        for (int i = 1; i < n - 1; i++) {
             leftMin[i] = left = Math.min(left, a[i]);
         }
         
-        for(int i = a.length - 2; i > 0; i--) {
+        for (int i = n - 2; i > 0; i--) {
             rightMin[i] = right = Math.min(right, a[i]);
         }
         
-        for(int i = 1; i < a.length - 1; i++) {
-            if(a[i] > leftMin[i] && a[i] > rightMin[i]) {
+        for (int i = 1; i < n - 1; i++) {
+            if (a[i] > leftMin[i] && a[i] > rightMin[i]) {
                 continue;
             }
+            
             answer++;
         }
         
