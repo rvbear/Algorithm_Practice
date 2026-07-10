91b086eac2b155b9ed0cd9f87be1a84acf81fb9f
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-10 23:12:34 +0900

    [level 3] Title: 순위, Time: 7.26 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,21 +1,22 @@
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        int[][] floyd = new int[n+1][n+1];
+        int[][] floyd = new int[n + 1][n + 1];
         
-        for(int i = 0; i < results.length; i++) {
+        for (int i = 0; i < results.length; i++) {
             floyd[results[i][0]][results[i][1]] = 1;
             floyd[results[i][1]][results[i][0]] = -1;
         }
         
-        for(int i = 0; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
-                for(int k = 1; k <= n; k++) {
-                    if(floyd[i][k] == 1 && floyd[k][j] == 1) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (floyd[i][k] == 1 && floyd[k][j] == 1) {
                         floyd[i][j] = 1;
                         floyd[j][i] = -1;
                     }
-                    if(floyd[i][k] == -1 && floyd[k][j] == -1) {
+                    
+                    if (floyd[i][k] == -1 && floyd[k][j] == -1) {
                         floyd[i][j] = -1;
                         floyd[j][i] = 1;
                     }
@@ -23,14 +24,15 @@ class Solution {
             }
         }
         
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             int count = 0;
-            for(int j = 1; j <= n; j++) {
-                if(floyd[i][j] != 0) {
+            for (int j = 1; j <= n; j++) {
+                if (floyd[i][j] != 0) {
                     count++;
                 }
             }
-            if(count == n-1) {
+            
+            if (count == n - 1) {
                 answer++;
             }
         }
