e07c8387f8bae5a4b75f4e1ca8c23c98336e6bc6
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-16 10:31:17 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 3.98 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -10,24 +10,27 @@ class Solution {
     String answer = null;
     
     public int getDistance(int x, int y, int r, int c) {
-        return Math.abs(x-r) + Math.abs(y-c);
+        return Math.abs(x - r) + Math.abs(y - c);
     }
     
     public void dfs(int x, int y, int r, int c, int depth, int k) {
-        if(answer != null || (depth + getDistance(x, y, r, c)) > k) {
+        if (answer != null || (depth + getDistance(x, y, r, c)) > k) {
             return;
         }
-        if(k == depth) {
+        
+        if (k == depth) {
             answer = stb.toString();
             return;
         }
-        for(int i = 0; i < 4; i++) {
+        
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if(nx <= arrRow && 0 < nx && ny <= arrCol && 0 < ny) {
+            
+            if (nx <= arrRow && 0 < nx && ny <= arrCol && 0 < ny) {
                 stb.append(str[i]);
-                dfs(nx, ny, r, c, depth+1, k);
-                stb.delete(depth, depth+1);
+                dfs(nx, ny, r, c, depth + 1, k);
+                stb.delete(depth, depth + 1);
             }
         }
     }
@@ -35,9 +38,9 @@ class Solution {
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         stb = new StringBuilder();
         arrRow = n; arrCol = m;
-        
         int distance = getDistance(x, y, r, c);
-        if((k-distance) % 2 == 1 || k < distance) {
+        
+        if ((k-distance) % 2 == 1 || k < distance) {
             return "impossible";
         }
         
