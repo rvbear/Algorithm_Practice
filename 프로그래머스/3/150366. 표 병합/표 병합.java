966d1f205b13ca2c5a5421cc47b6c0d772f956e4
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-23 10:02:00 +0900

    [level 3] Title: 표 병합, Time: 6.95 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/150366. 표 병합/표 병합.java
+++ 프로그래머스/3/150366. 표 병합/표 병합.java
@@ -1,48 +1,51 @@
 import java.util.*;
 
 class Solution {
-    public int[] parent = new int[2501];
-    public String[] value = new String[2501];
+    private int[] parent = new int[2501];
+    private String[] value = new String[2501];
     
-    public int find(int a) {
-        if(parent[a] == a) {
+    private int find(int a) {
+        if (parent[a] == a) {
             return a;
         } else {
             return parent[a] = find(parent[a]);
         }
     }
     
-    public void union(int a, int b) {
+    private void union(int a, int b) {
         a = find(a);
         b = find(b);
-        if(a != b) {
+        
+        if (a != b) {
             parent[b] = a;
         }
     }
     
-    public int convertNum(int x, int y) {
+    private int convertNum(int x, int y) {
         int result = 50 * (x - 1);
+        
         return result + y;
     }
     
     public String[] solution(String[] commands) {
-        for(int i = 1; i < 2501; i++) {
+        for (int i = 1; i < 2501; i++) {
             parent[i] = i;
             value[i] = "EMPTY";
         }
         
         List<String> answer = new ArrayList<>();
-        for(int i = 0; i < commands.length; i++) {
+        
+        for (int i = 0; i < commands.length; i++) {
             StringTokenizer stk = new StringTokenizer(commands[i]);
             String command = stk.nextToken();
             
-            if("UPDATE".equals(command)) {
-                if(stk.countTokens() == 2) {
+            if ("UPDATE".equals(command)) {
+                if (stk.countTokens() == 2) {
                     String before = stk.nextToken();
                     String after = stk.nextToken();
                     
-                    for(int j = 1; j < 2501; j++) {
-                        if(before.equals(value[j])) {
+                    for (int j = 1; j < 2501; j++) {
+                        if (before.equals(value[j])) {
                             value[j] = after;
                         }
                     }
@@ -55,7 +58,7 @@ class Solution {
                     
                     value[find(index)] = temp;
                 }
-            } else if("MERGE".equals(command)) {
+            } else if ("MERGE".equals(command)) {
                 int r1 = Integer.parseInt(stk.nextToken());
                 int c1 = Integer.parseInt(stk.nextToken());
                 int r2 = Integer.parseInt(stk.nextToken());
@@ -66,14 +69,14 @@ class Solution {
                 int root1 = find(index1);
                 int root2 = find(index2);
 
-                if(root1 != root2) {
+                if (root1 != root2) {
                     String rootValue = value[root1].equals("EMPTY") ? value[root2] : value[root1];
                     value[root1] = "EMPTY";
                     value[root2] = "EMPTY";
                     union(root1, root2);
                     value[root1] = rootValue;
                 }
-            } else if("UNMERGE".equals(command)) {
+            } else if ("UNMERGE".equals(command)) {
                 int r = Integer.parseInt(stk.nextToken());
                 int c = Integer.parseInt(stk.nextToken());
                 
@@ -85,16 +88,17 @@ class Solution {
                 value[index] = rootValue;
                 
                 List<Integer> deleteList = new ArrayList<>();
-                for(int j = 1; j < 2501; j++) {
-                    if(find(j) == root) {
+                
+                for (int j = 1; j < 2501; j++) {
+                    if (find(j) == root) {
                         deleteList.add(j);
                     }
                 }
                 
-                for(int t : deleteList) {
+                for (int t : deleteList) {
                     parent[t] = t;
                 }
-            } else if("PRINT".equals(command)) {
+            } else if ("PRINT".equals(command)) {
                 int r = Integer.parseInt(stk.nextToken());
                 int c = Integer.parseInt(stk.nextToken());
                 int index = convertNum(r, c);
