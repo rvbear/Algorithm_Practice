f0fd247ee5a60820e4ca96636b1e2e9dd13773fd
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-24 10:07:53 +0900

    [level 3] Title: 카운트 다운, Time: 34.71 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -1,22 +1,26 @@
 class Solution {
-    int[][] dp;
-    int[] solve(int n) {
-        if(dp[n][0] == Integer.MAX_VALUE) {
-            if(n >= 50) {
+    private int[][] dp;
+    
+    private int[] solve(int n) {
+        if (dp[n][0] == Integer.MAX_VALUE) {
+            if (n >= 50) {
                 int[] temp = solve(n - 50);
-                if((temp[0] + 1 < dp[n][0]) || ((temp[0] + 1 == dp[n][0]) && (temp[1] + 1 > dp[n][1]))) {
+                
+                if ((temp[0] + 1 < dp[n][0]) || ((temp[0] + 1 == dp[n][0]) && (temp[1] + 1 > dp[n][1]))) {
                     dp[n][0] = temp[0] + 1;
                     dp[n][1] = temp[1] + 1;
                 }
             }
             
             int start = n >= 20 ? 20 : n;
-            for(int i = start; i >= 1; i--) {
-                for(int j = 1; j <= 3; j++) {
-                    if(n >= i * j) {
+            
+            for (int i = start; i >= 1; i--) {
+                for (int j = 1; j <= 3; j++) {
+                    if (n >= i * j) {
                         int[] temp = solve(n - i * j);
                         int cnt = j == 1 ? 1 : 0;
-                        if(temp[0] + 1 < dp[n][0] || (temp[0] + 1 == dp[n][0] && (temp[1] + cnt > dp[n][1]))) {
+                        
+                        if (temp[0] + 1 < dp[n][0] || (temp[0] + 1 == dp[n][0] && (temp[1] + cnt > dp[n][1]))) {
                             dp[n][0] = temp[0] + 1;
                             dp[n][1] = temp[1] + cnt;
                         }
@@ -27,11 +31,12 @@ class Solution {
         
         return dp[n];
     }
+    
     public int[] solution(int target) {
         int[] answer = new int[2];
-        dp = new int[target+1][2];
+        dp = new int[target + 1][2];
         
-        for(int i = 1; i < target+1; i++) {
+        for (int i = 1; i < target + 1; i++) {
             dp[i][0] = Integer.MAX_VALUE;
         }
         
