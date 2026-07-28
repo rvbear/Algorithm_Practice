2ef38957ab3f286c8539ec3ade7b769a269419e4
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-28 10:32:59 +0900

    [level 3] Title: 억억단을 외우자, Time: 297.31 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.java
@@ -1,22 +1,22 @@
 class Solution {
     public int[] solution(int e, int[] starts) {
         int[] answer = new int[starts.length];
-        int[] count = new int[e+1];
+        int[] count = new int[e + 1];
         
-        for(int i = 1; i < e+1; i++) {
-            for(int j = 1; j <= e/i; j++) {
-                count[i*j]++;
+        for (int i = 1; i < e + 1; i++) {
+            for (int j = 1; j <= e / i; j++) {
+                count[i * j]++;
             }
         }
 
-        int[] max = new int[e+1];
+        int[] max = new int[e + 1];
         max[e] = e;
         
-        for(int i = e-1; i > 0; i--) {
-            max[i] = (count[i] >= count[max[i+1]]) ? i : max[i+1];
+        for (int i = e - 1; i > 0; i--) {
+            max[i] = (count[i] >= count[max[i + 1]]) ? i : max[i + 1];
         }
 
-        for(int i = 0; i < answer.length; i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = max[starts[i]];
         }
         
