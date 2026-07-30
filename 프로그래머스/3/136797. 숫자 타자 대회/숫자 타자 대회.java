d8cf0d67da7a08a0071ccf0dcdf0c6aee26f6d0c
KyungMin_Kim <107176570+rvbear@users.noreply.github.com>
2026-07-30 23:02:56 +0900

    [level 3] Title: 숫자 타자 대회, Time: 270.42 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.java
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    int[][] cost = {
+    private int[][] cost = {
         {1, 7, 6, 7, 5, 4, 5, 3, 2, 3},
         {7, 1, 2, 4, 2, 3, 5, 4, 5, 6},
         {6, 2, 1, 2, 3, 2, 3, 5, 4, 5},
@@ -13,27 +13,28 @@ class Solution {
         {2, 5, 4, 5, 3, 2, 3, 2, 1, 2},
         {3, 6, 5, 4, 5, 3, 2, 4, 2, 1}
     };
-    int[][][] dp;
-    String arr;
-    int n;
+    private int[][][] dp;
+    private String arr;
+    private int n;
     
-    public int solve(int idx, int left, int right) {
-        if(idx == n) {
+    private int solve(int idx, int left, int right) {
+        if (idx == n) {
             return 0;
         }
         
-        if(dp[idx][left][right] != -1) {
+        if (dp[idx][left][right] != -1) {
             return dp[idx][left][right];
         }
         
         int num = arr.charAt(idx) - '0';
         int res = Integer.MAX_VALUE;
         
-        if(num != right) {
-            res = Math.min(solve(idx+1, num, right) + cost[left][num], res);
+        if (num != right) {
+            res = Math.min(solve(idx + 1, num, right) + cost[left][num], res);
         }
-        if(num != left) {
-            res = Math.min(solve(idx+1, left, num) + cost[num][right], res);
+        
+        if (num != left) {
+            res = Math.min(solve(idx + 1, left, num) + cost[num][right], res);
         }
         
         return dp[idx][left][right] = res;
@@ -42,10 +43,10 @@ class Solution {
     public int solution(String numbers) {
         arr = numbers;
         n = numbers.length();
-        dp = new int[n+1][10][10];
+        dp = new int[n + 1][10][10];
         
-        for(int i = 0; i < n+1; i++) {
-            for(int j = 0; j < 10; j++) {
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < 10; j++) {
                 Arrays.fill(dp[i][j], -1);
             }
         }
